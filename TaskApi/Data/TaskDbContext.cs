8072ebf2036147b17d4ee62f1a7b817c8bf88b89
CesarAlejandroMA <cesmartinez@uniboyaca.edu.co>
2025-07-04 21:45:26 -0500

    Agregar archivos de proyecto.

---
+++ TaskApi/Data/TaskDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskApi.Models;
+
+namespace TaskApi.Data
+{
+    public class TaskDbContext : DbContext
+    {
+        public TaskDbContext(DbContextOptions<TaskDbContext> options)
+            : base(options) { }
+
+        public DbSet<TaskItem> TaskItems => Set<TaskItem>();
+    }
+}
---
+++ TaskApi/Models/TaskItem.cs
@@ -0,0 +1,11 @@
+﻿namespace TaskApi.Models
+{
+    public class TaskItem
+    {
+
+        public int Id { get; set; }               // PK autoincremental
+        public string Title { get; set; } = null!; // requerido
+        public bool IsCompleted { get; set; }
+
+    }
+}
